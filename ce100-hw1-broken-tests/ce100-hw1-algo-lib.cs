83244a09a4d431caa3fcfb861f4a3d17e5946ad0
Bushra KURT <bushra.kurt@outlook.com>
2023-03-08 14:49:44 +0300

    ce100-hw1-algo-lib: Remove unnecessary `using`s
    ```
    IDE0005: Using directive is unnecessary.
    ```
    
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-broken-tests/ce100-hw1-algo-lib.cs
+++ ce100-hw1-broken-tests/ce100-hw1-algo-lib.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ce100_hw1_broken_tests
 {
     public class ce100_hw1_algo_lib

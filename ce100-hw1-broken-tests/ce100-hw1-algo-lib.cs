43374c8d15d5b92795633be8ce902aaf50f3a3ac
Bushra KURT <bushra.kurt@outlook.com>
2023-03-08 15:14:44 +0300

    ce100-hw1-algo-lib: Implement recursive MergeSort
    https://www.programmingalgorithms.com/algorithm/merge-sort-recursive/
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-broken-tests/ce100-hw1-algo-lib.cs
+++ ce100-hw1-broken-tests/ce100-hw1-algo-lib.cs
@@ -32,5 +32,78 @@ namespace ce100_hw1_broken_tests
             // array.
             return arr;
         }
+
+        public static int[] MergeSortRecursive(ref int[] data, int left, int right)
+        {
+            if (left < right)
+            {
+                // Make a temporary variable
+                // named "m" and let it be the
+                // value it needs to be.
+                int m = left + (right - left) / 2;
+
+                // Call itself twice with one
+                // having left, and the other
+                // having right half thrown at it.
+                MergeSortRecursive(ref data, left, m);
+                MergeSortRecursive(ref data, m + 1, right);
+
+                // Conquer and merge.
+                Merge(ref data, left, m, right);
+            }
+
+            // Lastly, return the data.
+            return data;
+        }
+
+        // MergeSort implementation
+        private static void Merge(ref int[] data, int left, int mid, int right)
+        {
+            int i, j, k;
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (i = 0; i < n1; i++)
+                L[i] = data[left + i];
+
+            for (j = 0; j < n2; j++)
+                R[j] = data[mid + 1 + j];
+
+            i = 0;
+            j = 0;
+            k = left;
+
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    data[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    data[k] = R[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i < n1)
+            {
+                data[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                data[k] = R[j];
+                j++;
+                k++;
+            }
+        }
     }
 }

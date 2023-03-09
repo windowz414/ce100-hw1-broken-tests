56567377342ec8df6ad6c14fe661a5238abcc9af
Bushra KURT <bushra.kurt@outlook.com>
2023-03-09 19:05:43 +0300

    ce100-hw1-algo-lib: Implement binary searching
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-broken-tests/ce100-hw1-algo-lib.cs
+++ ce100-hw1-broken-tests/ce100-hw1-algo-lib.cs
@@ -221,5 +221,52 @@ namespace ce100_hw1_broken_tests
             // return the index of the pivot element
             return i + 1;
         }
+
+        public static object BinarySearchIterative(int[] inputArray, int key)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return "Nil";
+        }
+
+        public static object BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+        {
+            if (min > max)
+            {
+                return "Nil";
+            }
+            else
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    return BinarySearchRecursive(inputArray, key, min, mid - 1);
+                }
+                else
+                {
+                    return BinarySearchRecursive(inputArray, key, mid + 1, max);
+                }
+            }
+        }
     }
 }

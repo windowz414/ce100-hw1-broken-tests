237da8fcc9e02c592f8b5c762fa4fe83ebaed223
Bushra KURT <bushra.kurt@outlook.com>
2023-03-11 15:22:50 +0300

    ce100-hw1-algo-test: Implement unit test for Lomuto's Quick Sort
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
+++ ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
@@ -73,6 +73,22 @@ namespace ce100_hw1_algo_test_cs
         [TestMethod]
         public void TestLomutoQuickSort()
         {
+            // Generate 10k random inputs
+            int[] arr = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next();
+            }
+
+            // Sort the array using Lomuto's Quicksort
+            int[] sortedArr = ce100_hw1_algo_lib.LomutoQuickSort(arr, 0, arr.Length - 1);
+
+            // Check that the array is sorted
+            for (int i = 0; i < sortedArr.Length - 1; i++)
+            {
+                Assert.IsTrue(sortedArr[i] <= sortedArr[i + 1]);
+            }
         }
 
         [TestMethod]

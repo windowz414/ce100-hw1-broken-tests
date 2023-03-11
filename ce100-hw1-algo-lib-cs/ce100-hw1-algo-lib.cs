5aa8aef0eee3acb8b224c472595e17458ab40a82
Bushra KURT <bushra.kurt@outlook.com>
2023-03-11 21:15:45 +0300

    ce100-hw1-algo-lib: Initial Doxygen bringup
    * Also update some comments and protection level of some impls.
    
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-algo-lib-cs/ce100-hw1-algo-lib.cs
+++ ce100-hw1-algo-lib-cs/ce100-hw1-algo-lib.cs
@@ -4,6 +4,12 @@ namespace ce100_hw1_algo_lib_cs
 {
     public class ce100_hw1_algo_lib
     {
+        /**
+         * This function sorts an array of integers using selection sort method.
+         *
+         * @param arr An array of integers to sort.
+         * @return An array of integers containing the sorted elements.
+         */
         public static int[] SelectionSort(int[] arr)
         {
             // Set length of the received
@@ -35,6 +41,14 @@ namespace ce100_hw1_algo_lib_cs
             return arr;
         }
 
+        /**
+         * This function sorts an array of integers using recursive merge sort method.
+         *
+         * @param data An array of integers to sort.
+         * @param left The index of the leftmost element to sort.
+         * @param right The index of the rightmost element to sort.
+         * @return An array of integers containing the sorted elements.
+         */
         public static int[] MergeSortRecursive(ref int[] data, int left, int right)
         {
             if (left < right)
@@ -58,7 +72,7 @@ namespace ce100_hw1_algo_lib_cs
             return data;
         }
 
-        // MergeSort implementation
+        // Helper function for MergeSortRecursive. This MUST NOT be documented!
         private static void Merge(ref int[] data, int left, int mid, int right)
         {
             int i, j, k;
@@ -108,7 +122,14 @@ namespace ce100_hw1_algo_lib_cs
             }
         }
 
-        // Quick Sort with Hoare's partitioning.
+        /**
+         * This function sorts an array of integers using Hoare partition scheme and Quick Sort algorithm.
+         *
+         * @param arr An array of integers to sort.
+         * @param low The index of the leftmost element to sort.
+         * @param high The index of the rightmost element to sort.
+         * @return An array of integers containing the sorted elements.
+         */
         public static int[] HoareQuickSort(int[] arr, int low, int high)
         {
             if (low < high)
@@ -126,7 +147,8 @@ namespace ce100_hw1_algo_lib_cs
             return arr;
         }
 
-        static int HoarePartition(int[] arr, int low, int high)
+        // Helper function for HoareQuickSort. This MUST NOT be documented!
+        private static int HoarePartition(int[] arr, int low, int high)
         {
             // Generate a random pivot index.
             Random rand = new Random();
@@ -170,14 +192,24 @@ namespace ce100_hw1_algo_lib_cs
             }
         }
 
+        /**
+         * This function sorts an array of integers using Lomuto partition scheme and Quick Sort algorithm.
+         *
+         * @param arr An array of integers to sort.
+         * @param low The index of the leftmost element to sort.
+         * @param high The index of the rightmost element to sort.
+         * @return An array of integers containing the sorted elements.
+         */
         public static int[] LomutoQuickSort(int[] arr, int low, int high)
         {
             if (low < high)
             {
-                // partition the array and get the pivot index
+                // Partition the array and get the pivot
+                // index.
                 int pi = LomutoPartition(arr, low, high);
 
-                // recursively sort the left and right subarrays
+                // Recursively sort the left and right
+                // subarrays.
                 LomutoQuickSort(arr, low, pi - 1);
                 LomutoQuickSort(arr, pi + 1, high);
             }
@@ -185,27 +217,28 @@ namespace ce100_hw1_algo_lib_cs
             return arr;
         }
 
-        static int LomutoPartition(int[] arr, int low, int high)
+        // This is a helper function for LomutoQuickSort function. This MUST NOT be documented!
+        private static int LomutoPartition(int[] arr, int low, int high)
         {
-            // generate a random pivot index
+            // Generate a random pivot index.
             Random rand = new Random();
             int pivotIndex = rand.Next(low, high);
             int pivot = arr[pivotIndex];
 
-            // move the pivot element to the end of the array
+            // Move the pivot element to the end of the array.
             int temp1 = arr[pivotIndex];
             arr[pivotIndex] = arr[high];
             arr[high] = temp1;
 
-            // initialize the partition index
+            // Initialize the partition index.
             int i = low - 1;
 
-            // loop through the array from low to high-1
+            // Loop through the array from low to high-1.
             for (int j = low; j < high; j++)
             {
-                // if the current element is less than or equal
+                // If the current element is less than or equal
                 // to the pivot, swap it with the element at the
-                // partition index
+                // partition index.
                 if (arr[j] <= pivot)
                 {
                     i++;
@@ -215,15 +248,22 @@ namespace ce100_hw1_algo_lib_cs
                 }
             }
 
-            // swap the pivot element back to its final position
+            // Swap the pivot element back to its final position.
             int temp3 = arr[i + 1];
             arr[i + 1] = arr[high];
             arr[high] = temp3;
 
-            // return the index of the pivot element
+            // Return the index of the pivot element.
             return i + 1;
         }
 
+        /**
+         * Performs a binary search on a sorted array using an iterative approach.
+         *
+         * @param inputArray The sorted array to search.
+         * @param key The value to search for in the array.
+         * @return The index of the matching element in the array, or the string "Nil" if no match is found.
+         */
         public static object BinarySearchIterative(int[] inputArray, int key)
         {
             int min = 0;
@@ -247,6 +287,15 @@ namespace ce100_hw1_algo_lib_cs
             return "Nil";
         }
 
+        /**
+         * Performs a binary search on a sorted array using a recursive approach.
+         *
+         * @param inputArray The sorted array to search.
+         * @param key The value to search for in the array.
+         * @param min The minimum index of the range to search.
+         * @param max The maximum index of the range to search.
+         * @return The index of the matching element in the array, or the string "Nil" if no match is found.
+         */
         public static object BinarySearchRecursive(int[] inputArray, int key, int min, int max)
         {
             if (min > max)

f3d95fe251b6100206e56b8e57d05bdc8b2f5cac
Bushra KURT <bushra.kurt@outlook.com>
2023-03-11 14:04:30 +0300

    CE100-HW1: Initial unit test project
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

---
+++ ce100-hw1-algo-test-cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ce100-hw1-algo-test-cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ce100-hw1-algo-test-cs")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a2e85c9-82d0-4813-a52c-60ce72462f46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ce100_hw1_algo_test_cs
+{
+    [TestClass]
+    public class ce100_hw1_algo_test
+    {
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+        }
+
+        [TestMethod]
+        public void TestMergeSortRecursive()
+        {
+        }
+
+        [TestMethod]
+        public void TestHoareQuickSort()
+        {
+        }
+
+        [TestMethod]
+        public void TestLomutoQuickSort()
+        {
+        }
+
+        [TestMethod]
+        public void TestBinarySearchIterative()
+        {
+        }
+
+        [TestMethod]
+        public void TestBinarySearchRecursive()
+        {
+        }
+
+        [TestMethod]
+        public void TestIterativeMatricMultiplication()
+        {
+        }
+
+        [TestMethod]
+        public void TestRecursiveMatrixMultiplication()
+        {
+        }
+
+        [TestMethod]
+        public void TestMatrixMultiplicationStrassen()
+        {
+        }
+    }
+}

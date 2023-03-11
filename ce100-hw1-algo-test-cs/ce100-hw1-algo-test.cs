9c9c9dd47e445ede0a8789b95fded14d93d35700
Bushra KURT <bushra.kurt@outlook.com>
2023-03-11 14:50:37 +0300

    ce100-hw1-algo-test: Write unit test for selection sort
    * 10k inputs as requested! :D
    
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
+++ ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using ce100_hw1_algo_lib_cs;
 
 namespace ce100_hw1_algo_test_cs
 {
@@ -9,6 +10,22 @@ namespace ce100_hw1_algo_test_cs
         [TestMethod]
         public void TestSelectionSort()
         {
+            // Arrange
+            int[] arr = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next();
+            }
+
+            // Act
+            int[] sortedArr = ce100_hw1_algo_lib.SelectionSort(arr);
+
+            // Assert
+            for (int i = 0; i < sortedArr.Length - 1; i++)
+            {
+                Assert.IsTrue(sortedArr[i] <= sortedArr[i + 1]);
+            }
         }
 
         [TestMethod]

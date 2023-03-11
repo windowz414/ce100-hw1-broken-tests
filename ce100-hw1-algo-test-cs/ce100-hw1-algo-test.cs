52ed7a1ec90fc2685f5fa579215654bfff4279e2
Bushra KURT <bushra.kurt@outlook.com>
2023-03-11 15:18:34 +0300

    ce100-hw1-algo-test: Implement unit test for Hoare's Quick Sort
    Signed-off-by: Bushra KURT <bushra.kurt@outlook.com>

--- ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
+++ ce100-hw1-algo-test-cs/ce100-hw1-algo-test.cs
@@ -52,6 +52,22 @@ namespace ce100_hw1_algo_test_cs
         [TestMethod]
         public void TestHoareQuickSort()
         {
+            // Arrange
+            int[] arr = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(0, 10000);
+            }
+
+            // Act
+            int[] sortedArr = ce100_hw1_algo_lib.HoareQuickSort(arr, 0, arr.Length - 1);
+
+            // Assert
+            for (int i = 1; i < sortedArr.Length; i++)
+            {
+                Assert.IsTrue(sortedArr[i] >= sortedArr[i - 1]);
+            }
         }
 
         [TestMethod]
